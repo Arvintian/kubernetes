58c61092b3606ce485ab8cc8d0c858b8b1f1d792
Arvin <arvintian8@gmail.com>
2022-07-15 14:54:36 +0800

    v1.22.11-20220715

--- staging/src/k8s.io/component-base/version/base.go
+++ staging/src/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "22"
-	gitVersion   = "v1.22.11-k3s1"
-	gitCommit    = "e210e996cf393c3830af15af24e9e3429add5d1c"
+	gitVersion   = "v1.22.11-20220715"
+	gitCommit    = "3cb87937f92a357b75f643aeee42af7990e8fdb1"
 	gitTreeState = "clean"
-	buildDate = "2022-06-21T21:30:06Z"
+	buildDate = "2022-07-15T06:49:35Z"
 )
